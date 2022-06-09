84cb287fb5b566d1fc921cff1520eb0baf3f38cf
Marco Sérvio <marcoservio22@hotmail.com>
2022-06-09 15:22:44 -0300

    Primeiros passos para desenvolvimento web

---
+++ projetos/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Meu Primeiro site em PHP! WOOHOO!</title>
+    </head>
+    <body>
+        <?php
+            for($i = 0; $i < 10; $i++){
+                print("Linha número "  . $i . "<br />");
+            }
+            ?>
+    </body>
+</html>
